333514e2d47bdefd2e844a59f85e22a369ef104b
esteparian <info.esteparia@gmail.com>
2025-04-01 17:10:34 -0300

    Add translations/en.php 1743538233174

---
+++ translations/en.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'site_title' => 'Esteparian - Technology Solutions',
+    'home' => 'Home',
+    'services' => 'Services',
+    'industries' => 'Industries',
+    'about' => 'About Us',
+    'contact' => 'Contact',
+    'language' => 'Language',
+    'welcome' => 'Welcome to Esteparian'
+];
