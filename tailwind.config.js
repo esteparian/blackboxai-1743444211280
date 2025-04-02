67d419bb6d5e6ef6ff64fc24c78e9f611fb559bd
esteparian <info.esteparia@gmail.com>
2025-04-01 21:44:44 -0300

    Add tailwind.config.js 1743554683810

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./**/*.php",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#003366',
+        secondary: '#0077CC', 
+        accent: '#FFA500'
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
